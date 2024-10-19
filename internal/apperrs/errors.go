3e7627753895a602d4498bf47ca27363fb5f5d41
Ivan Arevkov <ivan.arevkov@gmail.com>
2024-10-19 20:55:36 +0300

    feat(apperrs) added sentinel errors

---
+++ internal/apperrs/errors.go
@@ -0,0 +1,10 @@
+package apperrs
+
+import "errors"
+
+var (
+	ErrAlreadyExist       = errors.New("already exist")
+	ErrNotFound           = errors.New("not found")
+	ErrConditionViolation = errors.New("condition violation")
+	ErrUnauthorized        = errors.New("unauthorized")
+)
